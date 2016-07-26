a491cf465eb510ee267f50fe9013aef41debd3f9
ulisesmx <mx.ulises@gmail.com>
2016-07-25 23:29:14 -0500

    2016-07-25: Camel case problem (SR: 97%) and String construction problem (SR: 86%) in strings.

---
+++ strings/camel_case.py
@@ -0,0 +1,3 @@
+#!/bin/python
+
+print (len([c for c in raw_input() if "A" <= c <= "Z"]) + 1)
---
+++ strings/string_construction.py
@@ -0,0 +1,5 @@
+#!/bin/python
+from sets import Set
+
+for _ in xrange(int(raw_input())):
+    print len(Set(list(raw_input())))
