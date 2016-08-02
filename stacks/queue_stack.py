eeb4ed080d3f790082d83e55bf87dcee6f1c329a
ulisesmx <mx.ulises@gmail.com>
2016-08-01 23:06:20 -0500

    2016-08-01: Modification in the Queue Stack problem solution making it faster (CTCI, SR: 80%)

--- stacks/queue_stack.py
+++ stacks/queue_stack.py
@@ -3,18 +3,41 @@
 class QueueStack(object):
 
     def __init__(self):
-        self.enq = []
-        self.deq = []
+        self.newest = []
+        self.oldest = []
 
-    def enqueue(self, value):
-        while self.deq:
-            self.enq.append(self.deq.pop())
-        self.enq.append(value)
+    def enqueue(self, data):
+        self.newest.append(data)
+
+
+    def _prepare_oldest(self):
+        if not self.oldest:
+            while self.newest:
+                self.oldest.append(self.newest.pop())
 
 
     def dequeue(self):
-        while self.enq:
-            self.deq.append(self.enq.pop())
-        if self.deq:
-            return self.deq.pop()
+        self._prepare_oldest()
+        if self.oldest:
+            return self.oldest.pop()
         raise Exception("Empty queue")
+
+
+    def front(self):
+        self._prepare_oldest()
+        if self.oldest:
+            return self.oldest[-1]
+        raise Exception("Empty queue")
+
+
+t = int(raw_input())
+queue = QueueStack()
+
+for _ in xrange(t):
+    query = map(int, raw_input().split())
+    if query[0] == 1:
+        queue.enqueue(query[1])
+    if query[0] == 2:
+        queue.dequeue()
+    if query[0] == 3:
+        print queue.front()
