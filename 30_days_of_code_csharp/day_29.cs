c3bf7e31f41fd780f6c7970b4e9567922ed851da
ulisesmx <mx.ulises@gmail.com>
2016-09-13 22:07:42 -0500

    2016-09-13: 30 days of code (C++), day 29

--- 30_days_of_code_csharp/day_29.cs
+++ 30_days_of_code_csharp/day_29.cs
@@ -1,32 +1,24 @@
 using System;
-using System.Collections.Generic;
-using System.Collections;
 using System.IO;
-using System.Linq;
 
 class Solution {
+
     static void Main(String[] args) {
         int t = Convert.ToInt32(Console.ReadLine());
-        while (t > 0){
-            string[] entry = Console.ReadLine().Split(' ');
-            int n = Convert.ToInt32(entry[0]);
-            int k = Convert.ToInt32(entry[1]);
-            ArrayList candidates = new ArrayList();
-            int min = 1;
-            while (min <= n) min = (min << 1) + 1;
-            min = min >> 1;
-            for (int i = min; i <= n; i++) candidates.Add(i);
-            int max = 0;
-            for(int i = k - 1; i > 0; i--){
-                foreach (int candidate in candidates) {
-                    int val = i & candidate;
-                    if (i < candidate && val > max) max = val;
-                    if (max == i) break;
+        for(int a0 = 0; a0 < t; a0++){
+            string[] tokens_n = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(tokens_n[0]);
+            int k = Convert.ToInt32(tokens_n[1]);
+            int maximal = 0;
+            for (int i = 0; i < n; i ++) {
+                for (int j = i + 1; j < n + 1; j++) {
+                    int val = i & j;
+                    if (maximal < val && val < k) {
+                        maximal = val;
+                    }
                 }
-                if (max == i) break;
             }
-            Console.WriteLine(max);
-            t--;
+            Console.WriteLine(maximal);
         }
     }
 }
