c28ad7fac898d65ac4e3569d109307fbbb61a4cd
ulisesmx <mx.ulises@gmail.com>
2016-10-02 23:58:45 -0500

    2016-10-02: Corrections an example in Resizable Array data structure

---
+++ data_structures/resizable_array/main_test.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "resizable_array.cpp"
+
+using namespace std;
+
+void print_array(ResizableArray<int> *A) {
+    cout << "Current size: " << A->get_size() << endl;
+    for (int i = 0; i < A->get_size(); i++) {
+        cout << A->get(i) << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    ResizableArray<int> A = ResizableArray<int>();
+    A.insert(1);
+    A.insert(2);
+    A.insert(3);
+    A.insert(4);
+    A.insert(5);
+    print_array(&A);
+    A.insert(7);
+    A.insert(8);
+    A.insert(9);
+    print_array(&A);
+    A.insert(10);
+    A.insert(13);
+    A.insert(12);
+    print_array(&A);
+    return 0;
+}
--- data_structures/resizable_array/resizable_array.cpp
+++ data_structures/resizable_array/resizable_array.cpp
@@ -13,22 +13,28 @@ template <class T> class ResizableArray {
 
     private:
         int size = 0;
-        int lenght = 16;
-        T container[lenght];
+        int lenght = 1;
+        T *container;
 
         void resize_container(){
             if (this->size == this->lenght) {
-                T new_container[lenght * 2];
+                T *new_container = new T[lenght * 2];
                 for (int i = 0; i < this->lenght; i++) {
                     new_container[i] = this->container[i];
                 }
                 this->container = new_container;
                 this->lenght = this->lenght * 2;
+                cout << "[INFO] Resize, new length: " << this->lenght << endl;
             }
         }
 
 
     public:
+
+        ResizableArray<T>() {
+            this->container = new T[lenght];
+        }
+
         int get_size() {
             return this->size;
         }
@@ -51,6 +57,7 @@ template <class T> class ResizableArray {
 
 
         void insert(T item) {
+            cout << "[INFO] Inserting: " << item << endl;
             this->resize_container();
             this->container[this->size] = item;
             this->size++;
