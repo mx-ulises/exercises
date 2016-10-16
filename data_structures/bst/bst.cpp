a806b55b57812589f675b5630270e1cbbe09a6fa
ulisesmx <mx.ulises@gmail.com>
2016-10-15 21:44:19 -0500

    2016-10-15: Binary Search Tree (BST) data structure in C++

--- data_structures/bst/bst.cpp
+++ data_structures/bst/bst.cpp
@@ -1,27 +1,71 @@
+#include <iostream>
+
+using namespace std;
+
 template <class T> class Node {
-	public:
-		T data;
-		Node<T>* left;
-		Node<T>* right;
+    public:
+        T data;
+        Node<T>* left;
+        Node<T>* right;
 
-		Node<T>(T data) {
-			this->data = data;
-		}
+        Node<T>(T data) {
+            this->data = data;
+        }
 
-		void insert(T data) {
-			if (data < this->data) {
-				if (this->left != NULL) {
-					this->left->insert(data);
-				} else {
-					this->left = new Node<T>(data);
-				}
-			} else if (this->data < data) {
-				if (this->right != NULL) {
-					this->right->insert(data);
-				} else {
-					this->right = new Node<T>(data);
-				}
-			}
-		}
+        void insert(T data) {
+            if (data < this->data) {
+                if (left == NULL) left = new Node<T>(data);
+                else left->insert(data);
+            } else if (this->data < data) {
+                if (right == NULL) right = new Node<T>(data);
+                else right->insert(data);
+            }
+        }
+
+        Node<T> find(T data) {
+            if (data == this->data) {
+                return this;
+            } else if (data < this->data) {
+                if (left == NULL) return NULL;
+                else return left->find(data);
+            } else {
+                if (right == NULL) return NULL;
+                else return right->find(data);
+            }
+        }
+
+        Node<T> max() {
+            Node<T>* current = this;
+            while (current->right != NULL) {
+                current = current->right;
+            }
+            return current;
+        }
+
+        Node<T> min() {
+            Node<T>* current = this;
+            while (current->left != NULL) {
+                current = current->left;
+            }
+            return current;
+        }
+
+        void inorder() {
+            if (left != NULL) left->inorder();
+            cout << data << " ";
+            if (right != NULL) right->inorder();
+        }
+
+        void preorder() {
+            cout << data << " ";
+            if (left != NULL) left->preorder();
+            if (right != NULL) right->preorder();
+        }
+
+        void posorder() {
+            if (left != NULL) left->posorder();
+            if (right != NULL) right->posorder();
+            cout << data << " ";
+        }
 
 };
