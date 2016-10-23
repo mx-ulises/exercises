00c740a7afeae396c695e049eb296d9007fe2c7b
ulisesmx <mx.ulises@gmail.com>
2016-10-22 19:19:02 -0500

    2016-10-22: Character combination problem with Dynamic programming, just get the number of combinations

--- dynamic_programming/char_combinations.py
+++ dynamic_programming/char_combinations.py
@@ -1,5 +1,7 @@
 #!/bin/python
 
+from sets import Set
+
 VALID_NUMS = {str(i + 1): chr(97 + i) for i in xrange(26)}
 
 def get_char(n_1, n_2 = ""):
@@ -9,6 +11,7 @@ def get_char(n_1, n_2 = ""):
     return None
 
 
+# Get all combinations
 def get_char_comb(num):
     num = list(num)
     c = get_char(num[0])
@@ -32,4 +35,24 @@ def get_comb(num):
         return []
     return get_char_comb(str(num))
 
-print get_comb(12121212)
+
+# Just get the num of combinations
+def get_char_comb_2(num):
+    num = list(num)
+    c = get_char(num[0])
+    prefix = 1
+    prev_prefix = 1
+    for i in xrange(1, len(num)):
+        c = get_char(num[i])
+        new_prefix = prefix
+        c = get_char(num[i - 1], num[i])
+        if c:
+            new_prefix += prev_prefix
+        prev_prefix = prefix
+        prefix = new_prefix
+    return prefix
+
+def get_comb_2(num):
+    if num <= 0:
+        return []
+    return get_char_comb_2(str(num))
