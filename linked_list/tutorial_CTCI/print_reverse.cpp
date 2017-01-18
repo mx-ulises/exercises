eb259f03548c17d92e859ecb3e5c347cddd8d729
ulisesmx <mx.ulises@gmail.com>
2017-01-17 23:01:44 -0600

    2017-01-17: Print Reverse method in Linked List tutorial

---
+++ linked_list/tutorial_CTCI/print_reverse.cpp
@@ -0,0 +1,11 @@
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+void ReversePrint(Node *head)
+{
+    if (head == NULL) return;
+    ReversePrint(head->next);
+    cout << head->data << endl;
+}
