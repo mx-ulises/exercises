9fffebbccb16bc07660b2af6f7a7954b6eede5c5
ulisesmx <mx.ulises@gmail.com>
2019-01-08 07:29:22 -0800

    2019-01-08: Binary Tree Height problem with Recursion (HR: 96%)

---
+++ hackerrank/height_of_binary_tree.py
@@ -0,0 +1,52 @@
+#!/bin/python
+
+class Node:
+    def __init__(self, info):
+        self.info = info
+        self.left = None
+        self.right = None
+        self.level = None
+
+    def __str__(self):
+        return str(self.info)
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def create(self, val):
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+def height(root):
+    if root == None:
+        return -1
+    return max(height(root.left), height(root.right)) + 1
+
+
+tree = BinarySearchTree()
+t = int(raw_input())
+
+arr = list(map(int, raw_input().split()))
+
+for i in xrange(t):
+    tree.create(arr[i])
+
+print height(tree.root)
